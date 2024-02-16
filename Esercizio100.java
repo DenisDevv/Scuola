1182ee7d6dfb08b4f93d86eb5c3fdfbcd215ad0b
Denis <67103577+DenisDevv@users.noreply.github.com>
2024-02-16 12:34:38 +0100

    Terminato il lavoro
    Completate le parti restanti + fix vari

--- Esercizio100.java
+++ Esercizio100.java
@@ -2,41 +2,74 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
 public class Esercizio100 {
-    public static void main(String[]args) {
-        Boolean scelta = false;
-        Scanner in = new Scanner(System.in);
-        Random rand = new Random();
-        int input;
-        System.out.println("Inserisci la dimensione array: ");
-        input = in.nextInt();
-        int numero;
-        int[] arrayUtente = new int[input];
-    do {
-        System.out.println("Seleziona un opzione dell'array: ");
-        System.out.println("1 - Carica l'array manualmente");
-        System.out.println("2 - Carica l'array in maniera casuale");
-        System.out.println("3 - Visualizza l'array");
-        System.out.println("4 - Cancella un elemento");
-        System.out.println("5 - Cancella tutte le occorrenze di un elemento");
-        System.out.println("6 - Esci");
-        input = in.nextInt();
-        switch (input){
-            case 1: {
-                for (int i = 0; i < arrayUtente.length; i++) {
-                    System.out.println("Inserisci il valore #" + (i+1));
-                    input = in.nextInt();
-                    arrayUtente[i] = input;
-                }
-                break;
-            }
-            case 2: {
-                for (int i = 0; i < arrayUtente.length; i++) {
-                    numero = rand.nextInt();
-                    arrayUtente[i] = input;
-                }
-                break;
-            }
-        }
-    } while(scelta);
-    }
+		public static void main(String[]args) {
+				Boolean scelta = true;
+				Scanner in = new Scanner(System.in);
+				Random rand = new Random();
+				int input;
+				System.out.println("Inserisci la dimensione array: ");
+				input = in.nextInt();
+				int numero;
+				int[] arrayUtente = new int[input];
+		do {
+                System.out.println("");
+                System.out.println("");
+				System.out.println("Seleziona un opzione dell'array: ");
+				System.out.println("1 - Carica l'array manualmente");
+				System.out.println("2 - Carica l'array in maniera casuale");
+				System.out.println("3 - Visualizza l'array");
+				System.out.println("4 - Cancella un elemento");
+				System.out.println("5 - Cancella tutte le occorrenze di un elemento");
+				System.out.println("6 - Esci");
+				input = in.nextInt();
+				switch (input){
+						case 1: {
+								for (int i = 0; i < arrayUtente.length; i++) {
+										System.out.println("Inserisci il valore #" + (i+1));
+										input = in.nextInt();
+										arrayUtente[i] = input;
+								}
+								break;
+						}
+						case 2: {
+								for (int i = 0; i < arrayUtente.length; i++) {
+										numero = rand.nextInt();
+										arrayUtente[i] = numero;
+								}
+								break;
+						}
+						case 3: {
+								System.out.println("");
+								System.out.print("Ecco gli elementi dell'array: ");
+								for (int elemento : arrayUtente) {
+										System.out.print(elemento + "; ");
+								}
+								break;
+						}
+						case 4: {
+								System.out.println("Inserisci la cella da sostituire con 0 (" + 0 + " - " + (arrayUtente.length - 1) + ")");
+								input = in.nextInt();
+								arrayUtente[input] = 0;
+								break;
+						}
+						case 5: {
+								System.out.println("Inserisci il numero da sostituire con 0");
+								input = in.nextInt();
+								for (int i = 0; i < arrayUtente.length; i++) {
+                                    if (arrayUtente[i] == input) {
+										arrayUtente[i] = 0;
+                                    }
+								}
+								break;
+						}
+						case 6: {
+								scelta = false;
+						}
+						default: {
+								break;
+						}
+				}
+		} while(scelta);
+        in.close();
+		}
 }
